4a47c25b2f5dac3c753afe2a098ee6fa47086fd5
dba <david.baetge@gmail.com>
2022-01-05 18:07:11 +0100

    docs - Added github-pages action.

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('postcss-import')({
-      path: '_sass'
+      path: ['_sass', 'node_modules']
     }),
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('tailwindcss')('./tailwind.config.js'),
