78a92d227e6948d6f9002050be205c5d75cb2d9b
dba <david.baetge@gmail.com>
2022-01-06 02:05:50 +0100

    docs - Updated workflow.

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  syntax: require('postcss-scss'),
-  parser: require('postcss-scss'),
   plugins: [
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('postcss-import')({
-      path: ['_sass', 'node_modules']
+      path: ['node_modules']
     }),
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('tailwindcss')('./tailwind.config.js'),
