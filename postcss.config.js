c95b66a8cc7ed171d6159c940a686ef943484a31
Daveiano <Daveiano@users.noreply.github.com>
2022-01-08 11:45:45 +0000

    deploy: 45537e3b29030a06cb0b62e1152358021ed8df15

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require('postcss-import')({
+      path: ['node_modules']
+    }),
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require('tailwindcss')('./tailwind.config.js'),
+    require('autoprefixer'),
+  ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // This is a bit nasty but ensures that everything works also in the gh-actions environment where the config lies
+  // in the root.
+  content: ["./**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('tailwind-container-break-out')]
+}
