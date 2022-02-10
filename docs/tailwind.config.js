d98b84b830b508fa795f2e38021b206faafe94ac
dba <david.baetge@gmail.com>
2022-02-10 01:48:31 +0100

    Updated docs - added jekyll-seo-tag, updated all pages, added darkmode.

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
+  darkMode: 'class',
+  theme: {
+    variants: {
+      extend: {
+        typography: ['dark']
+      }
+    },
+  },
   // This is a bit nasty but ensures that everything works also in the gh-actions environment where the config lies
   // in the root.
   content: ["./**/*.html"],
-  theme: {
-    extend: {
-
-    },
-  },
   plugins: [
     require('tailwind-container-break-out'),
     require('@tailwindcss/typography'),
-  ]
+  ],
 }
